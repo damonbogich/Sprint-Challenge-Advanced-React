50a4ac54e03c289b9d1491ef51f5a9154644fd70
damonbogich <damonbogich@gmail.com>
2020-01-17 14:03:49 -0500

    just added in the self study questions to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -40,3 +40,17 @@ class App extends React.Component {
 }
 
 export default App;
+
+
+//Self study questions:
+
+//1. You might use class components because you work at a company whose whole codebase is in class components and they don't want to refactor.
+
+//2. Life cycle methods and their purpose:
+// 2a. Birth/Mountin phase: Phase when the component is being built from the ground up
+// 2b. Growth/Updating phase: You're updating component data in this phase
+// 2c. Death/Unmounting phase: component is removed from screen
+
+//3. Purpose of custom hook: The purpose is to build hooks that you can re use throughout your components.  Things you could use them for include handling controlled inputs, managing event listeners, or watching for key presses.
+
+//4. Why is it important to test our apps?: It's important because we're human and will make mistakes.  Testing provides a safety net against those mistakes.
